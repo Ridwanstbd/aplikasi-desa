046a0522532432fe94970e5c66edc2f41b0c7a90
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-19 14:22:44 +0700

    fix

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -135,28 +135,28 @@
     <div id="carouselEkspedisi" class="carousel slide mb-2" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <div class="d-flex justify-items-center">
-                <div class="">
+            <div class="row justify-content-center">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/jne.png') }}" alt="Image 1">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/jnt.png') }}" alt="Image 2">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/sicepat.png') }}" alt="Image 3">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/ninja.png') }}" alt="Image 4">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/grab.png') }}" alt="Image 5">
                     </div>
@@ -164,28 +164,28 @@
             </div>
         </div>
         <div class="carousel-item">
-            <div class="d-flex justify-items-center">
-                <div class="">
+            <div class="row justify-content-center">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/gosend.png') }}" alt="Image 6">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/gokil.png') }}" alt="Image 7">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/jtr.png') }}" alt="Image 8">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/indah.png') }}" alt="Image 9">
                     </div>
                 </div>
-                <div class="">
+                <div class="col-2">
                     <div class="image-container">
                         <img src="{{ asset('assets/img/jnt_cargo.png') }}" alt="Image 10">
                     </div>
