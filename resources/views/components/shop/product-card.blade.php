4a257ce6ce561ea6887ab70ed28de15c4332d810
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-20 16:25:54 +0700

    fix: title product in product card

--- resources/views/components/shop/product-card.blade.php
+++ resources/views/components/shop/product-card.blade.php
@@ -5,7 +5,7 @@
     <div class="p-1">
         <a href="{{ route('products.show', $product->slug) }}" class="text-black text-decoration-none">
             <h5 class="fs-5">Rp{{ number_format($product->hargaTermurah, 0, ',', '.') }}</h5>
-            <p class="fs-6" style="color: green;" >{{ $product->name }}</p>
+            <p class="fs-6 text-truncate mb-0" style="color: green;" >{{ $product->name }}</p>
         </a>
         <div class="row justify-content-between align-items-center">
             <div class="col-4">
