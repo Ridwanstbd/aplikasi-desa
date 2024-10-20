c8b22f65267219ea82d7d23ee64af6e0556e3f37
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-20 19:25:05 +0700

    dev

--- resources/views/components/shop/scripts.blade.php
+++ resources/views/components/shop/scripts.blade.php
@@ -27,8 +27,7 @@
     const product = products.find(p => p.id === nextLead.product_id);
 
     if (!product) {
-        console.log(product.id);
-        console.error('Product not found');
+        console.log($products);
         return;
     }
 
