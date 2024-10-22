f50133aca1b9d09859264a6793f82d728a886a20
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-23 00:13:20 +0700

    fix

--- resources/views/components/shop/product-card.blade.php
+++ resources/views/components/shop/product-card.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ route('products.show', $product->slug) }}" class="position-relative">
     <img src="{{ Storage::url($product->main_image) }}" style="width: 100%; height: auto;" alt="{{ $product->name }}">
-    <span class="badge bg-primary position-absolute" style="bottom: -90px; left: 5px;">Bebas Biaya Admin</span>
-    <span class="badge bg-danger position-absolute" style="bottom: -90px; left: 125px;">COD</span>
+    <span class="badge bg-primary position-absolute admin-badge">Bebas Biaya Admin</span>
+    <span class="badge bg-danger position-absolute cod-badge">COD</span> <!-- Badge COD -->
 </a>
 <div class="p-1">
     <a href="{{ route('products.show', $product->slug) }}" class="text-black text-decoration-none">
@@ -19,3 +19,37 @@
         </div>
     </div>
 </div>
+
+<!-- Tambahkan style CSS di bawah ini -->
+<style>
+    .cod-badge {
+        bottom: -55px; /* Default untuk smartphone */
+        left: 125px; /* Default untuk smartphone */
+    }
+    .admin-badge {
+        bottom: -55px; /* Default untuk smartphone */
+        left: 125px; /* Default untuk smartphone */
+    }
+
+    @media (min-width: 768px) {
+        .cod-badge {
+            bottom: -90px; /* Tablet */
+            left: 125px;
+        }
+        .admin-badge {
+        bottom: -90px; /* Default untuk smartphone */
+        left: 5px; /* Default untuk smartphone */
+    }
+    }
+
+    @media (min-width: 992px) {
+        .cod-badge {
+            bottom: -83px; /* Desktop */
+            left: 125px;
+        }
+        .admin-badge {
+        bottom: -83px; /* Default untuk smartphone */
+        left: 5px; /* Default untuk smartphone */
+    }
+    }
+</style>
