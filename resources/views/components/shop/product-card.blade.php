3af2293840233baab721eeab4a0ab1e3d34b0b3d
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-22 23:46:19 +0700

    update: add badge card product

--- resources/views/components/shop/product-card.blade.php
+++ resources/views/components/shop/product-card.blade.php
@@ -1,18 +1,19 @@
-<a href="{{ route('products.show', $product->slug) }}" class="">
-    <img src="{{ Storage::url($product->main_image) }}" style="width:100%; height:auto;" alt="...">
+<a href="{{ route('products.show', $product->slug) }}" class="position-relative">
+    <img src="{{ Storage::url($product->main_image) }}" style="width: 100%; height: auto;" alt="{{ $product->name }}">
+    <span class="badge bg-primary position-absolute" style="bottom: 10px; left: 10px;">Bebas Biaya Admin</span>
 </a>
 <div class="p-1">
     <a href="{{ route('products.show', $product->slug) }}" class="text-black text-decoration-none">
         <h5 class="fs-5">Rp{{ number_format($product->hargaTermurah, 0, ',', '.') }}</h5>
-        <p class="fs-6" style="color: green;" >{{ $product->name }}</p>
+        <p class="fs-6" style="color: green;">{{ $product->name }}</p>
     </a>
     <div class="row w-100 justify-content-between align-items-center">
-        <div class="col-4">
+        <div class="col-auto">
             <div onclick="shareProduct('{{ route('products.show', $product->slug) }}', '{{ $product->name }}')" class="d-flex align-items-center" style="cursor: pointer;">
                 <i class="fa-solid fa-share text-gray-400"></i>
             </div>
         </div>
-        <div class="col-8">
+        <div class="col-auto text-end">
             <a href="{{ route('products.show', $product->slug) }}" class="btn btn-warning" style="white-space: nowrap;">Lihat detail</a>
         </div>
     </div>
