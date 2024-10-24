0662e211229ee58e0251a9019a836413e170f367
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-24 21:42:02 +0700

    fix: fixing bootstrap

--- resources/views/pages/admin/products/show.blade.php
+++ resources/views/pages/admin/products/show.blade.php
@@ -169,7 +169,6 @@
     </style>
 @endpush
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     function showAlert(icon, title, text){
             Swal.fire({
