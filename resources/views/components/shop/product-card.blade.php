7eda83d4bd25806596973137d3c6310e43e67cb3
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-24 10:26:46 +0700

    fix: fixing position badge

--- resources/views/components/shop/product-card.blade.php
+++ resources/views/components/shop/product-card.blade.php
@@ -1,9 +1,11 @@
 <a href="{{ route('products.show', $product->slug) }}" class="position-relative">
     <img src="{{ Storage::url($product->main_image) }}" style="width: 100%; height: auto;" alt="{{ $product->name }}">
-    <span class="badge bg-primary position-absolute admin-badge">Bebas Biaya Admin</span>
-    <span class="badge bg-danger position-absolute cod-badge">COD</span> <!-- Badge COD -->
 </a>
 <div class="p-1">
+    <div class="d-flex">
+        <span class="badge bg-primary position-absolute">Bebas Biaya Admin</span>
+        <span class="badge bg-danger position-absolute">COD</span>
+    </div>
     <a href="{{ route('products.show', $product->slug) }}" class="text-black text-decoration-none">
         <h5 class="fs-5">Rp{{ number_format($product->hargaTermurah, 0, ',', '.') }}</h5>
         <p class="fs-6" style="color: green;">{{ $product->name }}</p>
@@ -19,37 +21,3 @@
         </div>
     </div>
 </div>
-
-<!-- Tambahkan style CSS di bawah ini -->
-<style>
-    .cod-badge {
-        bottom: -75px; /* Default untuk smartphone */
-        left: 125px; /* Default untuk smartphone */
-    }
-    .admin-badge {
-        bottom: -75px; /* Default untuk smartphone */
-        left: 5px; /* Default untuk smartphone */
-    }
-
-    @media (min-width: 768px) {
-        .cod-badge {
-            bottom: -84px; /* Tablet */
-            left: 125px;
-        }
-        .admin-badge {
-        bottom: -84px; /* Default untuk smartphone */
-        left: 5px; /* Default untuk smartphone */
-    }
-    }
-
-    @media (min-width: 992px) {
-        .cod-badge {
-            bottom: -83px; /* Desktop */
-            left: 125px;
-        }
-        .admin-badge {
-        bottom: -83px; /* Default untuk smartphone */
-        left: 5px; /* Default untuk smartphone */
-    }
-    }
-</style>
