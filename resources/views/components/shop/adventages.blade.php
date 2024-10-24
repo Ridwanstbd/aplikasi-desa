1a71681e5e00e7c4c164c47f6680a741db82a194
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-24 20:51:07 +0700

    fix: styling home fix2

--- resources/views/components/shop/adventages.blade.php
+++ resources/views/components/shop/adventages.blade.php
@@ -1,4 +1,4 @@
 <div class="pt-5">
-<h3 class="text-center py-5 w-100 text-white" style="background-color: #2a401a;">KEUNTUNGAN</h3>
-    img src="{{asset('assets/img/banner2.png')}}" class="img-fluid" alt="" srcset="">
+<h3 class="text-center py-2 my-1 w-100 text-white" style="background-color: #2a401a;">KEUNTUNGAN</h3>
+    <img src="{{asset('assets/img/banner2.png')}}" class="img-fluid" alt="" srcset="">
 </div>
--- resources/views/components/shop/shipping.blade.php
+++ resources/views/components/shop/shipping.blade.php
@@ -1,4 +1,4 @@
-<h3 class="text-center py-5 w-100 text-white" style="background-color: #2a401a;">JASA PENGIRIMAN</h3>
+<h3 class="text-center py-2 my-1 w-100 text-white" style="background-color: #2a401a;">JASA PENGIRIMAN</h3>
 <div id="carouselEkspedisi" class="carousel slide mb-2" data-ride="carousel">
     <div class="carousel-inner">
         <x-shop.shipping-carousel-items />
