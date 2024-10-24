6766d9660254b684eef6f172ce9b63f891a667d6
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-24 21:03:55 +0700

    fix: add product component in show product page

--- resources/views/pages/admin/products/show.blade.php
+++ resources/views/pages/admin/products/show.blade.php
@@ -88,27 +88,7 @@
     <div class="row mt-3">
         <h4>Produk Lainnya</h4>
         @foreach ($relatedProducts as $relatedProduct)
-        <div class="col-6 col-md-4 col-lg-2 mb-4">
-            <a href="{{ route('products.show', $relatedProduct->slug) }}" class="">
-                <img src="{{ Storage::url($relatedProduct->main_image) }}" style="width:11rem; height: 11rem;" alt="...">
-            </a>
-            <div class="p-1">
-                <a href="{{ route('products.show', $relatedProduct->slug) }}" class="text-black text-decoration-none">
-                <h5 class="fs-5">Rp{{ number_format($relatedProduct->hargaTermurah, 0, ',', '.') }}</h5>
-                <p class="fs-6" style="color: green;" >{{ $relatedProduct->name }}</p>
-                </a>
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-4">
-                    <div onclick="shareProduct('{{ route('products.show', $relatedProduct->slug) }}', '{{ $relatedProduct->name }}')" class="d-flex align-items-center" style="cursor: pointer;">
-                        <i class="fa-solid fa-share text-gray-400"></i> <span class="fs-8 text-gray-400">Bagikan</span>
-                    </div>
-                    </div>
-                    <div class="col-8">
-                        <a href="{{ route('products.show', $relatedProduct->slug) }}" class="btn btn-warning" style="white-space: nowrap;">Lihat detail</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-shop.product-card :product="$relatedProduct" />
         @endforeach
     </div>
     <!-- Modal Bagikan Produk -->
