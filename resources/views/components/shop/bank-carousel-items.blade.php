3c9eef42a0c05d6a40e2fc441764345b340eb661
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-10-26 17:42:50 +0700

    update: add bank transfer

---
+++ resources/views/components/shop/bank-carousel-items.blade.php
@@ -0,0 +1,38 @@
+<div class="carousel-item active">
+    <div class="row justify-content-center">
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/bca.png') }}" alt="Image 1">
+            </div>
+        </div>
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/bri.png') }}" alt="Image 2">
+            </div>
+        </div>
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/mandiri.png') }}" alt="Image 3">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="carousel-item">
+    <div class="row justify-content-center">
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/ovo.png') }}" alt="Image 6">
+            </div>
+        </div>
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/dana.png') }}" alt="Image 7">
+            </div>
+        </div>
+        <div class="col-3 col-md-2">
+            <div class="image-container">
+                <img src="{{ asset('assets/img/mandiri.png') }}" alt="Image 8">
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/shop/fake-sales.blade.php
+++
@@ -1,7 +0,0 @@
-<div id="product-alert">
-    <div id="product-info" class="product-info">
-        <img id="product-image" src="" alt="Product">
-        <div id="product-text"></div>
-    </div>
-    <button id="close-btn"><i class="bi bi-x-lg"></i></button>
-</div>
