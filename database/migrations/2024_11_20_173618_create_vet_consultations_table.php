3b9e9f7d1103d1ec2ac374ece93d750eaf613f39
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-11-22 23:18:53 +0700

    footer

--- database/migrations/2024_11_20_173618_create_vet_consultations_table.php
+++ database/migrations/2024_11_20_173618_create_vet_consultations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('vet_consultations', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->text('address');
+            $table->string('phone_number');
+            $table->date('consultation_date');
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
