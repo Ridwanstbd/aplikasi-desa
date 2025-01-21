f4a324829a14596612c6a224535a119d98e4b16a
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-01-21 23:44:55 +0700

    okelah

--- resources/views/partials/form.blade.php
+++
@@ -1,9 +0,0 @@
-<form class="{{$class}}" action="{{$action}}" id="{{$id}}" method="{{$method}}" enctype="{{$enctype}}" {{$attributes}} >
-    @csrf
-    @if ($method === 'POST' ||$method === 'PUT' || $method === 'PATCH' || $method === 'DELETE')
-        @method($method)
-    @endif
-    {{$slot}}
-</form>
-
-
