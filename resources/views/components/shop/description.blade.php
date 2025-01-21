9ff869f1fab027f444eb0f83fb1147f91f943a09
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-01-21 23:33:46 +0700

    [UPDATE] CLEANING PROJEK

--- resources/views/components/shop/description.blade.php
+++
@@ -1,6 +0,0 @@
-@if (!empty($shop->first()->description))
-    <div class="p-3 p-md-8 m-md-3 text-center">
-        <h2 class="fs-2">Selamat Datang di <span class="d-block d-md-inline">{{$shop->first()->name}}</span></h2>
-        <p class="fs-4">{{$shop->first()->description}}</p>
-    </div>
-@endif
--- resources/views/components/shop/marketplace.blade.php
+++
@@ -1,8 +0,0 @@
-<h3 class="text-center py-2 my-3 w-100 text-white" style="background-color: #2a401a;">MARKETPLACE</h3>
-<div class="d-flex flex-wrap justify-content-center gap-2 my-2">
-    @foreach ($marketplaces as $marketplace)
-        <div class="mb-2">
-            <x-marketplace-button type="{{$marketplace->type}}" url="{{$marketplace->marketplace_url}}" name="{{$marketplace->name}}" />
-        </div>
-    @endforeach
-</div>
